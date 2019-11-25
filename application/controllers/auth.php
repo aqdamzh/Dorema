b3df723bc4e13bc4a36c5d04e901f74e7cc92fa0
aqdamzh <aqdamzain1@gmail.com>
2019-11-25 14:50:36 +0700

    menambahkan fungsi logout

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -41,4 +41,9 @@ class Auth extends CI_Controller {
             }
         }
     }
+
+    public function logout(){
+        $this->session->sess_destroy();
+        redirect('auth/login');
+    }
 }
