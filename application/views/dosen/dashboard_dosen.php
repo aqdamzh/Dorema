147146fb711dcfd8bbde19b1d877322e1462f36d
aqdamzh <aqdamzain1@gmail.com>
2019-11-27 00:50:39 +0700

    some change

--- application/views/dosen/dashboard_dosen.php
+++ application/views/dosen/dashboard_dosen.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="<?php echo base_url() ?>assets/dist/img/DoremaLogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url() ?>assets/adminLTE/dist/img/DoremaLogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Dorema</span>
     </a>
