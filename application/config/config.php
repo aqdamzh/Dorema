43dbc3f9fc858b79856a3d87beeb77244c3a1148
aqdamzh <aqdamzain1@gmail.com>
2019-12-02 14:24:20 +0700

    config base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/dorema/';
+$config['base_url'] = 'http://latihandorema.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
