e865b4cb678f78bd9d246a4e89fb3fce537ddd81
ridart44 <ridarth@gmail.com>
2019-12-07 00:37:43 +0700

    Send Accept Email

--- application/controllers/dosen.php
+++ application/controllers/dosen.php
@@ -116,7 +116,42 @@ class Dosen extends CI_Controller {
 		$data = $this->model_pendaftar->view_pendaftar($register_id);
 
 		$this->model_project->update_data($where, $update, 'tb_pendaftar');
-		redirect('dosen/detail/'.$data->id_project);
+		$config = [
+			'protocol'  => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_user' => 'system.dorema@gmail.com',
+			'smtp_pass' => 'h4FE::80m',
+			'smtp_port' => 465,
+			'mailtype'  => 'html',
+			'charset'   => 'utf-8',
+			'newline'   => "\r\n"
+
+		];
+		$project = $this->model_project->data_project($data->id_project);
+		$project_name = $project->nama_project;
+		$this->load->library('email',$config);
+		$sender = $this->session->userdata('name');
+		$this->email->from('system.dorema@gmail.com',$sender);
+		$this->email->to('jhonrie033@gmail.com');
+		$this->email->subject($project_name);
+		$this->email->message(
+		'<center><b><h1>Congratulation</h1></b></center>'.
+		'Selamat anda telah diterima pada proyek '. $project_name . '.<br><br>' .
+		'Terima Kasih atas partisipasi dalam mengikuti seleksi pemilihan asisten untuk proyek '. 
+		$project_name . '.<br>Untuk informasi lebih lanjut silakan hubungi dosen terkait di Sekretariat Departemen.'.
+		'<br><br>Tertanda<br>'. $sender);
+		if($this->email->send())
+		{
+			redirect('dosen/detail/'.$data->id_project);
+			return true;
+		}
+		else
+		{
+			echo $this->email->print_debugger();
+			die;
+		}
+		
+		 
 	}
 
 	public function download_cv($cv){
