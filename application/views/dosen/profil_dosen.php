a6d61eec62a97f282144173fbe530a91590cb966
aqdamzh <aqdamzain1@gmail.com>
2019-12-08 19:55:10 +0700

    commit 9

--- application/views/dosen/profil_dosen.php
+++ application/views/dosen/profil_dosen.php
@@ -1,7 +1,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="<?php echo base_url() ?>mahasiswa/list" class="brand-link">
+        <a href="<?php echo base_url() ?>dosen/dashboard" class="brand-link">
         <img src="<?php echo base_url() ?>assets/adminLTE/dist/img/DoremaLogo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">Dorema</span>
@@ -34,7 +34,7 @@
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="<?php echo base_url() ?>mahasiswa/list">Home</a></li>
+                <li class="breadcrumb-item"><a href="<?php echo base_url() ?>dosen/dashboard">Home</a></li>
                 <li class="breadcrumb-item active">User Profile</li>
                 </ol>
             </div>
