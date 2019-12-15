0f6cf043578fc242b6c001ff100a6463b72d02c7
aqdamzh <aqdamzain1@gmail.com>
2019-12-16 04:51:23 +0700

    change form login

--- application/views/form_login.php
+++ application/views/form_login.php
@@ -29,37 +29,6 @@
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
-      <?php echo $this->session->flashdata('pesan') ?>
-      <form action="<?php echo base_url('auth/login') ?>" method="post">
-      <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Username"
-          name="username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-        </div>
-        <?php echo form_error('password', '<div class="text-danger 
-          small ml-2">','</div>'); ?>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password"
-          name="password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-dark btn-block 
-            form-control">Login</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
       <hr>
       <div class="row">
         <div class="col-4">
